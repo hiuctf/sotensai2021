23c8df11c61b0250d0027d6a020e66c42e413358
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-09-13 07:46:08 +0900

    [fix] CERT_OPTIONの修正

--- lets_encrypt/entrypoint.sh
+++ lets_encrypt/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "CERT_DOMAIN: $CERT_DOMAIN"
 echo certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN
-certbot certonly --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN \
+certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN \
 && cd /etc/letsencrypt/live/$CERT_DOMAIN/ \
 && cat fullchain.pem privkey.pem > server.pem
