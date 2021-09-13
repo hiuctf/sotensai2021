456fe8c6a305e6b1b96e64b5f39ecb42696292b8
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-09-13 09:24:51 +0900

    [add] 更新期限があればリクエストしない

--- lets_encrypt/entrypoint.sh
+++ lets_encrypt/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 echo "CERT_DOMAIN: $CERT_DOMAIN"
 echo certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN
-certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN \
-&& cd /etc/letsencrypt/live/$CERT_DOMAIN/ \
-&& cat fullchain.pem privkey.pem > server.pem
+LASTEXEC=$(cat /etc/letsencrypt/lastexec)
+TIME_NOW=$(date +%s)
+TIME_DIFF=$(expr $TIME_NOW - $LASTEXEC)
+echo last exec: $LASTEXEC, time now: $TIME_NOW
+if [ $TIME_DIFF -lt "86400" ] ; then
+  echo certfile is not expired
+else
+  certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN \
+  && cd /etc/letsencrypt/live/$CERT_DOMAIN/ \
+  && cat fullchain.pem privkey.pem > server.pem \
+  && date +%s > ../../lastexec
+fi
