d59d6283e2963add5098f77e4caccdaed04119ed
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-09-16 18:15:45 +0900

    [fix] 環境変数名をDOMAINからFQDNに変更

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # environments
 **required**<br>
 `CERT_EMAIL=your@mail.addr`<br>
-`CERT_DOMAIN=your.domain.name`<br>
+`CERT_FQDN=your.domain.name`<br>
 
 optional<br>
 `CERT_OPTION=--dry-run --some-options`<br>
--- lets_encrypt/entrypoint.sh
+++ lets_encrypt/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "CERT_DOMAIN: $CERT_DOMAIN"
-echo certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN
+echo certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_FQDN
 LASTEXEC=$(cat /etc/letsencrypt/lastexec)
 TIME_NOW=$(date +%s)
 TIME_DIFF=$(expr $TIME_NOW - $LASTEXEC)
@@ -8,8 +7,9 @@ echo last exec: $LASTEXEC, time now: $TIME_NOW
 if [ $TIME_DIFF -lt "86400" ] ; then
   echo certfile is not expired
 else
-  certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_DOMAIN \
-  && cd /etc/letsencrypt/live/$CERT_DOMAIN/ \
-  && cat fullchain.pem privkey.pem > server.pem \
-  && date +%s > ../../lastexec
+  certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_FQDN \
+  && date +%s > lastexec \
+  && chmod -R a+rx live \
+  && cd /etc/letsencrypt/live/$CERT_FQDN/ \
+  && cat fullchain.pem privkey.pem > server.pem
 fi
