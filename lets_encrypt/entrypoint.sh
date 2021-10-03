4c71edebf7a772f61b37a251368e80c701010d97
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-10-03 13:42:26 +0900

    [add] SNI利用の為の証明書格納ディレクトリを使わせる

--- lets_encrypt/entrypoint.sh
+++ lets_encrypt/entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
+echo "CERT_FQDN: $CERT_FQDN"
 echo certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_FQDN
 LASTEXEC=$(cat /etc/letsencrypt/lastexec)
 TIME_NOW=$(date +%s)
 TIME_DIFF=$(expr $TIME_NOW - $LASTEXEC)
 echo last exec: $LASTEXEC, time now: $TIME_NOW
-if [ $TIME_DIFF -lt "86400" ] ; then
+
+
+mkdir -p /etc/letsencrypt/certfiles
+chmod a+rx /etc/letsencrypt/$CERT_FQDN/live
+if [ $CERT_FQDN = "" ] ; then 
+  echo "env: \$CERT_FQDN not set!"
+  return 1
+fi
+
+if [ $TIME_DIFF -lt "864000" ] ; then
   echo certfile is not expired
 else
   certbot certonly $CERT_OPTION --agree-tos --standalone --email $CERT_EMAIL --non-interactive -d $CERT_FQDN \
   && date +%s > lastexec \
-  && chmod -R a+rx live \
   && cd /etc/letsencrypt/live/$CERT_FQDN/ \
-  && cat fullchain.pem privkey.pem > server.pem
+  && cat fullchain.pem privkey.pem > server.pem \
+  && ln -sf /etc/letsencrypt/$CERT_FQDN/live/server.pem /etc/letsencrypt/certfiles/server.pem
 fi
