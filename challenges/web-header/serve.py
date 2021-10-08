ea90bc124e2a7c8f644f15e127ceb28901a48aa9
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2021-10-09 02:26:29 +0900

    app fix

--- challenges/web-header/serve.py
+++ challenges/web-header/serve.py
@@ -1,18 +1,20 @@
 from flask import Flask, redirect, make_response, render_template, Blueprint
-app = Flask(__name__, static_folder="static/assets", static_url_path="/assets", url_prefix="/web/04")
-static_images = Blueprint("images", __name__, static_folder="static/images", static_url_path="/images")
-app.register_blueprint(static_images)
+app = Flask(__name__)
+app_main = Blueprint("main", __name__, url_prefix="/web/04", static_folder="static/assets", static_url_path="/assets")
+static_images = Blueprint("images", __name__, url_prefix="/web/04", static_folder="static/images", static_url_path="/images")
 
-@app.route("/")
+@app_main.route("/")
 def redirect_index():
     return redirect("/index.html")
 
-@app.route("/index.html")
+@app_main.route("/index.html")
 def index():
     resp = make_response(render_template("index.html"))
     resp.headers["Flag"] = "hiuctf{0k_g0_ah3ad}"
     return resp
 
+app.register_blueprint(app_main)
+app.register_blueprint(static_images)
 
 if __name__ == "__main__":
     print(app.url_map)
